6f10dab5c75d089f84ec29d66e6a10e248e074c2
stephaniesac <stephanie.a.chua@gmail.com>
2018-07-29 17:31:38 +1000

    Added more prints to EmployeeTester

--- week02/EmployeeTester.java
+++ week02/EmployeeTester.java
@@ -6,12 +6,17 @@ public class EmployeeTester {
     System.out.println("WEEK02 LAB: Employee & Manager Demo");
 
     Employee e = new Employee("Steph", 100);
-    Manager m = new Manager("RichSteph", 5000, LocalDate.now());
+    System.out.println(e.toString());
 
+    Manager m = new Manager("RichSteph", 5000, LocalDate.now());
+    System.out.println(m.toString());
 
     // Employee a = m;
-    // System.out.println(a.getClass());
+    // System.out.println("\nPrinting out details of Employee that is a Manager!");
+    // System.out.println("Class: " + a.getClass());
+    // System.out.println("Calling toString():");
+    // System.out.println(a.toString());
+    // Following line will not work because a is treated like an Employee
     // System.out.println(a.getHireDate());
-
   }
 }
